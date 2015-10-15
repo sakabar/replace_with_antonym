1661f73aed893c347a519912d4dd8d8a9f4694a4
sakaki <takafumi.sakakibara.pdx@gmail.com>
2015-10-15 17:50:39 +0900

    Modify: library name was old

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 import re
 import unittest
-import main
+import replace_lib
 
 class TestMain(unittest.TestCase):
     # def setUp(self):
@@ -15,17 +15,17 @@ class TestMain(unittest.TestCase):
     def test_search_lemma0(self):
         lemma = "好く"
         line = '(副詞 ((読み おりよく)(見出し語 折好く おり好く 折よく おりよく)(意味情報 "代表表記:折好く/おりよく")))'
-        self.assertFalse(main.search_lemma(lemma, line))
+        self.assertFalse(replace_lib.search_lemma(lemma, line))
 
     def test_search_lemma1(self):
         lemma = "来る"
         line = '(連体詞 ((読み きたるべき)(見出し語 来るべき きたるべき)(意味情報 "代表表記:来るべき/きたるべき")))'
-        self.assertFalse(main.search_lemma(lemma, line))
+        self.assertFalse(replace_lib.search_lemma(lemma, line))
 
     def test_search_lemma2(self):
         lemma = "来る"
         line = '(動詞 ((読み 来る)(見出し語 (来る 0.8))(活用型 カ変動詞来)(意味情報 "代表表記:来る/くる 反義:動詞:帰る/かえる")))'
-        self.assertTrue(main.search_lemma(lemma, line))
+        self.assertTrue(replace_lib.search_lemma(lemma, line))
 
 
 
@@ -42,7 +42,7 @@ class TestMain(unittest.TestCase):
 # """[1:-1].split('\n') #初めの改行をカット
         lemma = "好く"
         pos = "動詞"
-        self.assertEquals(main.get_katuyou_type(lemma, pos),"子音動詞カ行")
+        self.assertEquals(replace_lib.get_katuyou_type(lemma, pos),"子音動詞カ行")
 
 
 if __name__ == '__main__':
